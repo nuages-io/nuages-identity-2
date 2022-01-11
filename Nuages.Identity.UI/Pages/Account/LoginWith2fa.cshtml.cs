ae63deeaf53251966578c27d017f5be31f6a4651
martin <Wingman123*>
2022-01-11 18:38:48 -0500

    ok

--- Nuages.Identity.UI/Pages/Account/LoginWith2fa.cshtml.cs
+++ Nuages.Identity.UI/Pages/Account/LoginWith2fa.cshtml.cs
@@ -112,17 +112,16 @@ namespace Nuages.Identity.UI.Pages.Account
                 _logger.LogInformation("User with ID '{UserId}' logged in with 2fa.", user.Id);
                 return LocalRedirect(returnUrl);
             }
-            else if (result.IsLockedOut)
+
+            if (result.IsLockedOut)
             {
                 _logger.LogWarning("User with ID '{UserId}' account locked out.", user.Id);
                 return RedirectToPage("./Lockout");
             }
-            else
-            {
-                _logger.LogWarning("Invalid authenticator code entered for user with ID '{UserId}'.", user.Id);
-                ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
-                return Page();
-            }
+
+            _logger.LogWarning("Invalid authenticator code entered for user with ID '{UserId}'.", user.Id);
+            ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
+            return Page();
         }
     }
 }
