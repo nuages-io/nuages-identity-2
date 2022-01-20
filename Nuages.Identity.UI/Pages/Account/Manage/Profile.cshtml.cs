4f5f7126ca8d9e6721538c8256dd67fc119d53ee
martin <Wingman123*>
2022-01-20 09:49:31 -0500

    ok

---
+++ Nuages.Identity.UI/Pages/Account/Manage/Profile.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Nuages.Identity.UI.Pages.Account.Manage;
+
+public class Profile : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
