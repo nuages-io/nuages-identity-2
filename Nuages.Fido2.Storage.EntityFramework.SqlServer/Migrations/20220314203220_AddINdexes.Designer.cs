0c4a8c7ca959fc9077f2f2ef1fe6bb9a4b74df4c
Martin Massé <martin@nuages.org>
2022-03-14 16:37:55 -0400

    ok

--- Nuages.Fido2.Storage.EntityFramework.SqlServer/Migrations/20220314203220_AddINdexes.Designer.cs
+++ Nuages.Fido2.Storage.EntityFramework.SqlServer/Migrations/20220314203220_AddINdexes.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Nuages.Fido2.Storage.EntityFramework.SqlServer;
 
@@ -11,9 +12,10 @@ using Nuages.Fido2.Storage.EntityFramework.SqlServer;
 namespace Nuages.Fido2.Storage.EntityFramework.SqlServer.Migrations
 {
     [DbContext(typeof(IdentityFido2SqlServerDbContext))]
-    partial class IdentityFido2SqlServerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220314203220_AddINdexes")]
+    partial class AddINdexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Nuages.Fido2.Storage.EntityFramework/IdentityFido2DbContext.cs
+++ Nuages.Fido2.Storage.EntityFramework/IdentityFido2DbContext.cs
@@ -21,5 +21,13 @@ public class IdentityFido2DbContext : DbContext
             .HasIndex(c => new { c.UserIdBase64 });
         
         
+        modelBuilder.Entity<Fido2Credential>()
+            .HasIndex(c => new { c.UserHandleBase64 });
+        
+         modelBuilder.Entity<Fido2Credential>()
+            .HasIndex(c => new { c.DescriptorIdBase64 }).IsUnique();
+         
+         modelBuilder.Entity<Fido2Credential>()
+             .HasIndex(c => new { c.UserIdBase64, c.DescriptorIdBase64 });
     }
 }
