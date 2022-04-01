99278df782c6ec8f017217485490b43562dfccf0
Martin Massé <martin@nuages.org>
2022-04-01 07:13:39 -0400

    Move to src

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 #dotnet publish ./Nuages.Identity.API/Nuages.Identity.API.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
 
-npm install --prefix ./Nuages.Identity.UI
-dotnet publish ./Nuages.Identity.UI/Nuages.Identity.UI.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
+npm install --prefix ./src/Nuages.Identity.UI
+dotnet publish ./src/Nuages.Identity.UI/Nuages.Identity.UI.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
