2323af790898fc5082716a17ef1e877d4070b473
Martin Massé <martin@nuages.org>
2022-04-04 10:18:52 -0400

    Remove CDK

--- build.sh
+++
@@ -1,6 +0,0 @@
-
-
-#dotnet publish ./Nuages.Identity.API/Nuages.Identity.API.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
-
-npm install --prefix ./src/Nuages.Identity.UI
-dotnet publish ./src/Nuages.Identity.UI/Nuages.Identity.UI.csproj --configuration Release --framework net6.0 --self-contained false /p:GenerateRuntimeConfigurationFiles=true --runtime linux-x64
