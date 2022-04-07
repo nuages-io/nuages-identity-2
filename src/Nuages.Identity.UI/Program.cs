5df17b39647ad965d13f5d0a01d69cc110724929
Martin Massé <martin@nuages.org>
2022-04-07 14:46:20 -0400

    OK

--- src/Nuages.Identity.UI/Program.cs
+++ src/Nuages.Identity.UI/Program.cs
@@ -270,7 +270,11 @@ builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders =
         ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
 });
+
 
 services.AddHttpContextAccessor();
 
