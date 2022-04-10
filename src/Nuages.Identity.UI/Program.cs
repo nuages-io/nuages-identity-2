af8b9161925644329d0b8c6ccfe53d9166033b6c
Martin Massé <martin@nuages.org>
2022-04-09 21:49:03 -0400

    ok

--- src/Nuages.Identity.UI/Program.cs
+++ src/Nuages.Identity.UI/Program.cs
@@ -28,19 +28,19 @@ using Nuages.Localization.Storage.Config.Sources;
 using Nuages.Web;
 using OpenIddict.Abstractions;
 
-
-
 var builder = WebApplication.CreateBuilder(args);
 
 var configBuilder = builder.Configuration
     .AddJsonFile("appsettings.json", false, true);
 
-
-
 if (builder.Environment.IsDevelopment())
 {
     configBuilder.AddJsonFile("appsettings.local.json", false, true);
 }
+// else
+// {
+//     configBuilder.AddJsonFile("appsettings.prod.json", true, true);
+// }
 
 configBuilder.AddEnvironmentVariables();
 
