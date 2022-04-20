257fc0419c7197e816cb11b25d84c39a7b9612f6
Martin Massé <martin@nuages.org>
2022-04-20 13:06:04 -0400

    ok

--- src/Nuages.Identity.Services.AWS/IdentityAWSExtension.cs
+++ src/Nuages.Identity.Services.AWS/IdentityAWSExtension.cs
@@ -10,23 +10,23 @@ public static class IdentityAWSExtension
 {
     public static void AddAWS(this IServiceCollection services, IConfiguration configuration, string templateFileName, bool initializeTemplate = false)
     {
-        //Add options from Configuration if available
-        //https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/net-dg-config-netcore.html
-        services.AddDefaultAWSOptions(configuration.GetAWSOptions());
+        if (configuration.GetValue<bool>("Nuages:UseAWS"))
+        {
+            //Add options from Configuration if available
+            //https://docs.aws.amazon.com/sdk-for-net/v3/developer-guide/net-dg-config-netcore.html
+            services.AddDefaultAWSOptions(configuration.GetAWSOptions());
     
-        //Save Data Protection key to AWS SM Paramtere Store
-        services.AddDataProtection()
-            .PersistKeysToAWSSystemsManager("Nuages.Identity.UI/DataProtection");
+            //Save Data Protection key to AWS SM Paramtere Store
+            services.AddDataProtection()
+                .PersistKeysToAWSSystemsManager("Nuages.Identity.UI/DataProtection");
         
-        services.AddAWSSender(templateFileName, initializeTemplate);
+            services.AddAWSSender(templateFileName, initializeTemplate);
+        }
     }
 
     public static void LoadAWSConfiguration(this IConfigurationBuilder configBuilder, ConfigurationManager configManager)
     {
-         
-        var useAws = configManager.GetValue<bool>("Nuages:UseAWS");
-        
-        if (useAws)
+        if (configManager.GetValue<bool>("Nuages:UseAWS"))
         {
             var config = new ApplicationConfig();
             
