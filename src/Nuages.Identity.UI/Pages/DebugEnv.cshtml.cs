f88500e9b5501e9a904b60dde613adce5721bc70
Martin Massé <martin@nuages.org>
2022-11-14 21:04:19 +0000

    DeugEnv #2

--- src/Nuages.Identity.UI/Pages/DebugEnv.cshtml.cs
+++ src/Nuages.Identity.UI/Pages/DebugEnv.cshtml.cs
@@ -5,8 +5,15 @@ namespace Nuages.Identity.UI.Pages;
 
 public class DebugEnv : PageModel
 {
+    private readonly IConfiguration _configuration;
+
+    public DebugEnv(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     public List<Variable> EnvVariables { get; set; } = new List<Variable>();
-    
+    public List<Variable> Configs { get; set; } = new List<Variable>();
+
     public void OnGet()
     {
         foreach (DictionaryEntry v in System.Environment.GetEnvironmentVariables())
@@ -15,6 +22,15 @@ public class DebugEnv : PageModel
             {
                 Key = v.Key.ToString(),
                 Value = v.Value.ToString()
+            });
+        }
+        
+        foreach (var v in _configuration.AsEnumerable())
+        {
+            Configs.Add(new Variable
+            {
+                Key = v.Key,
+                Value = v.Value!
             });
         }
     }
